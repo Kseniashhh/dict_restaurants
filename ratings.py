c84da183c81ed6cd2771e4533cb455ffb2c0c84b
Hackbright Student <no-reply@hackbrightacademy.com>
2018-07-02 20:28:02 -0700

    user adding ratings works

--- ratings.py
+++ ratings.py
@@ -12,6 +12,21 @@ def rate_restaurant(filename):
 		restaurant, rating = line.split(":")
 		restaurant_dct[restaurant]=rating
 
+	asking = True
+	while asking:
+		ask_q = input("Do you want to add a new review? Y/N ").upper()
+		if ask_q == "Y":
+			ask_restaurant = input("Which restaurant do you want to rate? ").title()
+			if ask_restaurant in restaurant_dct:
+				print ("This restaurant already has a rating , it's {}".format(restaurant_dct[ask_restaurant]))
+				ask_rating = int(input("What would you like to replace the rating with? 1-5: "))
+			else:
+				ask_rating = int(input("this restaurant has never been rated. What would you like to rate it? 1-5: "))
+			restaurant_dct[ask_restaurant]=ask_rating
+		else:
+			print ("OK, all the ratings:\n")
+			asking = False
+
 	for key, value in sorted(restaurant_dct.items()):
 		print ("{} is rated at {}".format(key, value))
 
