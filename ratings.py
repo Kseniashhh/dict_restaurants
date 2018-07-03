ec3b96be4600192bdfa343093e1971703caf5314
Hackbright Student <no-reply@hackbrightacademy.com>
2018-07-02 21:01:11 -0700

    end of class with looped

--- ratings.py
+++ ratings.py
@@ -4,35 +4,73 @@
 # put your code here
 import sys
 
+menu = """Menu:
+		- (A)dd a restaurant
+		- (S)how the list of restaurants
+		- (Q)uit
+		"""
 
-def rate_restaurant(filename):
-	restaurant_dct={}
-	for line in  open(filename):
-		line = line.rstrip()
-		restaurant, rating = line.split(":")
-		restaurant_dct[restaurant]=rating
 
+def show_menu():
+	print (menu)
+
+
+def show_restaurants(dictionary):
+	for key, value in sorted(dictionary.items()):
+		print ("{} is rated at {}".format(key, value))
+
+
+
+
+def add_restaurant(dictionary):
 	asking = True
 	while asking:
-		ask_q = input("Do you want to add a new review? Y/N ").upper()
+		ask_q = input("Do you want to add a new review? Y/N ").upper().strip()
 		if ask_q == "Y":
 			ask_restaurant = input("Which restaurant do you want to rate? ").title()
-			if ask_restaurant in restaurant_dct:
-				print ("This restaurant already has a rating , it's {}".format(restaurant_dct[ask_restaurant]))
-				ask_rating = int(input("What would you like to replace the rating with? 1-5: "))
+			if ask_restaurant in dictionary:
+				print ("This restaurant already has a rating , it's {}".format(dictionary[ask_restaurant]))
+				ask_rating = round(float(input("What would you like to replace the rating with? 1-5: ")))
 			else:
-				ask_rating = int(input("this restaurant has never been rated. What would you like to rate it? 1-5: "))
-			restaurant_dct[ask_restaurant]=ask_rating
+				ask_rating = round(float(input("This restaurant has never been rated. What would you like to rate it? 1-5: ")))
+			
+			if ask_rating >=1 and ask_rating <= 5:
+				dictionary[ask_restaurant] = ask_rating
+				print ("You've successfully added {} with {} rating".format(ask_restaurant, ask_rating))
+			else:
+				print ("This is outside of the rating range. Please try again.")
 		else:
 			print ("OK, all the ratings:\n")
 			asking = False
 
-	for key, value in sorted(restaurant_dct.items()):
-		print ("{} is rated at {}".format(key, value))
+	return dictionary
+
+
+
+
+
+
+def run_restaurant_rating(filename):
+	restaurant_dct = {}
+	for line in open(filename):
+		line = line.rstrip()
+		restaurant, rating = line.split(":")
+		restaurant_dct[restaurant] = rating
+
+	showing_menu =True
+	while showing_menu:
+		show_menu()
+		choice = input("What would you like to do?: ").capitalize().strip()
+		if choice == "A":
+			restaurant_dct = add_restaurant(restaurant_dct)
+		elif choice == "S":
+			show_restaurants(restaurant_dct)
+		elif choice == "Q":
+			break
+		else:
+			print ("Don't recognize this option. PLease try again")
+			continue
 
 
 current_file = sys.argv[1]
-rate_restaurant(current_file)
-
-
-
+run_restaurant_rating(current_file)
